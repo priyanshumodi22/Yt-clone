106533296ae83a55189618882f13fca8f2b7c7eb
priyanshumodi22 <priyanshumodi2001@gmail.com>
2023-05-28 00:09:32 +0530

    leftNav_bug

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -26,9 +26,8 @@ const LeftNav = () => {
 
   return (
     <div
-      className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
-        mobileMenu ? "translate-x-0" : ""
-      }`}
+      className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all 
+      ${mobileMenu ? "translate-x-1" : ""}`}
     >
       <div className="flex px-5 flex-col">
         {categories.map((item) => {
@@ -50,9 +49,7 @@ const LeftNav = () => {
           );
         })}
         <hr className="my-5 border-white/[0.2]" />
-        <div className="text-white/[0.5] text-center text-[15px]">
-            Gro.care
-        </div>
+        <div className="text-white/[0.5] text-center text-[15px]">Gro.care</div>
       </div>
     </div>
   );
